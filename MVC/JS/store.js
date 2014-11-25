d22b63a4845dbc86e20a6cf72c347bf284dc84f3
Jackie88 <j.grzempowski@gmail.com>
2014-11-25 22:43:13 +0100

    Update store.js

--- MVC/JS/store.js
+++ MVC/JS/store.js
@@ -1,128 +1,24 @@
-window.addEventListener('DOMContentLoaded', function () {
-	//After the page structure is loaded
-
-	var store = new UAM.Store();
-
-	var inputView = new UAM.InputView(document.querySelector('#inputview'));
-	var listView = new UAM.ListView(document.querySelector('#listview'));
-	var footerView = new UAM.FooterView(document.querySelector('footer'));
-
-	new InputCtrl(inputView, store);
-	new ListCtrl(listView, store);
-	new FooterCtrl(footerView, store);
-});
-
-InputCtrl = function (inputView,store) {
-
-	var addElement = function(element) {
-		console.log(this);
-		store.add(element);
-	};
-
-	var updateView = function(text) {
-		inputView.clear(text);
-	};
-
-	inputView.on("addElement", addElement);
-	store.on("updateView", updateView);
-};
-
-
-UAM.InputView = function (inputView) {
+UAM.Store = function () {
 	UAM.EventEmitter.call(this);
 
-	var button= document.querySelector('#button');
-	var textInput = inputview
-
-	this.addElement = function() {
-		var text = textInput.value;
-		text = 'dddd';
-		if(text) {
-			console.log(this);
-			this.emit("addElement",text);
-		}
-		else
-			console.log('Nic nie wpisales');
-	};
-
-	this.clear = function() {
-		if(text) {
-			this.textInput.value = "";
-		}
-	}
-
-	button.addEventListener("click",this.addElement());
+	this.data  = [];
+	this.selected = 0;
 };
 
-UAM.utils.inherits(UAM.EventEmitter, UAM.InputView);
+UAM.utils.inherits(UAM.EventEmitter, UAM.Store);
 
-
-ListCtrl = function (listView,store) {
-
-	var drawElement = function(data) {
-		listView.drawElement(data);
-	}
-
-	var updateState = function(className) {
-		store.update(className);
-	}
-
-	store.on("elementAdded",drawElement);
-	listView.on("updateState", updateState);
+UAM.Store.prototype.add = function (data) {
+	this.data.push(data);
+	this.emit("elementAdded", data);
+	this.emit("updateView", data);
 };
 
+UAM.Store.prototype.update = function (data) {
 
-UAM.ListView = function (listView) {
-	UAM.EventEmitter.call(this);
+	if (data =='listItem' && this.selected>0)
+    	this.selected --;
+    else if (data =='doneItem')
+    	this.selected ++;
 
-	this.list= listView;
-
-	this.drawElement = function(data) {
-		element = document.createElement('li');
-    	element.innerHTML = data;
-      	element.className = 'listItem';
-
-    	element.addEventListener("click", this.updateState(element));
-
-    	this.list.appendChild(element);
-	};
-
-	this.updateState = function(element){
-    	if (element.className =='listItem')
-    		element.className == 'doneItem';
-    	else
-    		element.className =='listItem';
-
-    	this.emit("updateState", element.className);
-    }
-
+    this.emit("updateFooter", this.data.length, this.selected);
 };
-
-UAM.utils.inherits(UAM.EventEmitter, UAM.ListView);
-
-
-FooterCtrl = function (footerView,store) {
-
-	var updateData = function(all, done) {
-		footerView.updateCounter(all,done);
-	}
-
-	store.on("updateFooter",updateData);
-};
-
-UAM.FooterView = function (footerView) {
-	UAM.EventEmitter.call(this);
-
-	elementsCounter = document.querySelector('#all');
-	elementsDone = document.querySelector('#done');
-	elementsNotDone = document.querySelector('#notdone');
-
-	this.updateCounter = function(all, done) {
-		elementsCounter.value = all;
-		elementsDone.value = done;
-		elementsNotDone.value = all - done;
-	};
-
-};
-
-UAM.utils.inherits(UAM.EventEmitter, UAM.FooterView);
