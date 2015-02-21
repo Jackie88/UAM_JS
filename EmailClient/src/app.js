539f610489a7aec950790f0f71f592e9df4c5e19
Jackie88 <jacek@pc7.home>
2015-02-21 13:03:21 +0100

    statis html

--- EmailClient/src/app.js
+++ EmailClient/src/app.js
@@ -1,1 +1,13 @@
 // New file
+(function(){
+
+	var scotchApp = angular.module('emailClient', []);
+
+    // create the controller and inject Angular's $scope
+    scotchApp.controller('mainController', function($scope) {
+
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    });
+
+})();
