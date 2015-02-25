c137f22013acd3597cd2d793d720257e0e4b0a5c
Variuss <zolwik50@wp.pl>
2015-02-25 23:02:51 +0100

    commit 2

--- EmailClient/src/Configuration/ConfigController.js
+++ EmailClient/src/Configuration/ConfigController.js
@@ -1,11 +1,30 @@
 angular.module('email-config')
-	.controller('ConfigController', function($state,$scope){
+	.controller('ConfigController', function($state,$scope,localStorageService){
 	
 		$scope.$parent.title = "Config";
-	
 		
-		$scope.change = function(){
-			$scope.appTitle.style.background = 'red';	
-			$state.go('inbox');
+		$scope.change = function(int){
+			if (int == 1) {
+				$scope.appTitle.style.background = 'Red';
+				$scope.li.style.background = 'Salmon';
+				$state.go('config');
+			}
+			else if (int == 2) {
+				$scope.appTitle.style.background = 'Blue';
+				$scope.li.style.background = 'Aqua';				
+				$state.go('config');
+			}
+			else if (int == 3) {
+				$scope.appTitle.style.background = 'Green';	
+				$scope.li.style.background = 'PaleGreen';
+				$state.go('config');
+			}
 		};
+		
+		$scope.refresh_time = function(int){
+
+		};
+		
+		$scope.change();
+		$scope.refresh_time();
 });
